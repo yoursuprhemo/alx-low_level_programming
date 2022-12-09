390dbfda65da019f5d90e140a1ed4e61c5b36c94
yoursuprhemo <thesuprhemo@gmail.com>
2022-12-09 08:32:07 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - puts text
+ *
+ * Return: puts text 0 (Success)
+ */
+int main(void)
+{
+
+	puts("\"Programming is like buildi        ng a multilingual puzzle");
+	return (0);
+}
+
