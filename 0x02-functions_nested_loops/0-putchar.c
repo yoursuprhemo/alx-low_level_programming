a9f3a01747124e32657bc53a633e0450c769f813
yoursuprhemo <thesuprhemo@gmail.com>
2023-01-01 18:29:34 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - Prints putchar with an underscore at the beginning.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, xy;
+
+	xy = sizeof(str) / sizeof(int);
+	for (count = 0; count < xy; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
+}
